3e317446edc890b756d998b44cdf5c3ce4937d8b
xmruby <menksoft_liang@163.com>
2012-08-18 17:55:35 +0800

    功能基本完成
    查询数据有些bug需要处理

--- app/controllers/meter_day_datas_controller.rb
+++ app/controllers/meter_day_datas_controller.rb
@@ -1,6 +1,12 @@
 class MeterDayDatasController < ApplicationController
   def index
-    # @meter_days_data = MeterDayData.search("#{params[:m_id]}", "#{params[:start_date]}", "#{params[:end_date]}")
-    @meter_days_data = MeterDayData.search("#{params[:meter_id]}", "2012-07-26", "2012-08-02")
+    if (params[:start_date].nil? == true && params[:end_date].nil? == true)
+    	@meter_days_data = MeterDayData.search("#{params[:meter_id]}", "2012-07-26", "2012-08-02")
+    else
+    	@meter_days_data = MeterDayData.search("#{params[:meter_id]}", "#{params[:start_date]}", "#{params[:end_date]}")
+    end
+  end
+  def get_id
+  	@id = params[:meter_id]
   end
 end
--- app/models/meter_day_data.rb
+++ app/models/meter_day_data.rb
@@ -6,7 +6,7 @@ class MeterDayData < ActiveRecord::Base
   scope :by_date, ->(date) {where(" (SELECT CONVERT (VARCHAR(30), M_date, 23)) = ?", date)}
   scope :by_id_in_date, ->(m_id, start_date, end_date) {where("M_ID = ? AND M_date BETWEEN ? AND ?",m_id, start_date, end_date)}
   scope :by_id, ->(id) {where("M_Id = ?", id)}
-  
+
   def self.search(m_id, start_date, end_date)
   	start_date = Time.zone.parse(start_date) - 1.day
   	end_date = Time.zone.parse(end_date)
@@ -15,22 +15,30 @@ class MeterDayData < ActiveRecord::Base
   end
 
   # 获取 每日数据的 数组days_power[]
-  def self.get_days_power(datas, start_date, end_date)
+  def self.get_days_power(datas, start_date, end_date) 
   	days_power = []
+  	days_power << datas[0]
 
-  	while start_date <= end_date
-  		if get_day_data(datas, days_power, start_date) == false
-  		  days_power << { :real_z => 12, :m_date => start_date, :spare_power => 88}
-  		end
-	  	start_date = start_date + 1.day
-  	end
-  	day_power(days_power)
+  	for i in(1...datas.length) do
+  		if(datas[i][:M_date].to_date - datas[i-1][:M_date].to_date == 1)
+  			days_power << datas[i]
+			else
+				days = (datas[i][:M_date].to_date - datas[i-1][:M_date].to_date).to_i
+				avg_power = get_avg_power(datas[i-1], datas[i], days)
+				days_power << compensation_data(datas[i-1], days, avg_power)
+			end
+		end
+  	days_power = days_power.flatten
+  	get_finally_data(days_power, start_date, end_date)
   end
 
-  def self.day_power(days_power)
+  def self.get_finally_data(days_power, start_date, end_date)
   	datas = []
-		for i in(1...days_power.length) do  
-			datas << { :power => days_power[i][:real_z] - days_power[i-1][:real_z], :date => days_power[i][:m_date], :spare_power => days_power[i][:spare_power]}
+  	# for day in (start_date.to_date...end_date.to_date) do day
+  	# 	if()
+  	# end
+		for i in(1...days_power.length) do
+			datas << { :power => days_power[i][:Real_z] - days_power[i-1][:Real_z], :date => days_power[i][:M_date], :spare_power => days_power[i][:SparePwr], :total_power => days_power[i][:Real_z]}
 		end
 		datas
   end
@@ -46,11 +54,21 @@ class MeterDayData < ActiveRecord::Base
   	flag
   end
 
-  # 获取 某一天用电量
-  def self.get_day_power(m_id, day)
-  	scope = self.scoped
-      scope = scope.by_id(m_id)
-     scope = scope.by_date(day)
-    scope
+  def self.get_avg_power(pre_day_data, next_day_data, days)
+  	avg_power = (next_day_data[:Real_z] - pre_day_data[:Real_z]) / days
+  end
+
+  # 补数据 前一天数据、无数据隔天数、每日平均用电量
+  def self.compensation_data(pre_day_data, days, avg_power)
+  	array_data = []
+  	days.times.each { |v|
+  		hs = {}
+  		hs = Marshal.load(Marshal.dump(pre_day_data))
+  		hs[:Real_z] = pre_day_data[:Real_z] + (avg_power * (v + 1))
+  		hs[:M_date] = pre_day_data[:M_date] + (v + 1).day
+  		hs[:SparePwr] = pre_day_data[:SparePwr] - (avg_power * (v + 1))
+  		array_data << hs
+  	}
+  	array_data
   end
 end
